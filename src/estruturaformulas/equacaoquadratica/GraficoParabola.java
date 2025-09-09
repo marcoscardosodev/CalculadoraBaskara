2d5236ebd1a18db02efe8300776ae449a0075b7c
Marcos Cardoso Ferreira Marques <marcoscfm6@gmail.com>
2025-09-08 22:18:24 -0300

    Criação da Classe GraficoParabola, no qual a função é justamente mostrar o grafico da parabola

---
+++ src/estruturaformulas/equacaoquadratica/GraficoParabola.java
@@ -0,0 +1,60 @@
+package estruturaformulas.equacaoquadratica;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class GraficoParabola extends JFrame{
+
+    private Equacao equacao;
+    private ArrayList<Point> pontos;
+
+    public GraficoParabola (Equacao equacao){
+
+        this.equacao = equacao;
+        this.pontos = new ArrayList<>();
+
+        setTitle("Grafico Parabola");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        calcularPontos();
+    }
+    private void calcularPontos(){
+        double xVertice = equacao.getXVertice();
+
+        double inicio = xVertice - 5;
+        double fim = xVertice + 5;
+        double passo = (fim - inicio) / 200;
+
+        for (double x = inicio; x <= fim; x += passo){
+            double y = (equacao.getA() * x * x) + (equacao.getB() * x) + equacao.getC();
+            pontos.add(new Point((int) (x * 50), (int) (y * -50)));
+
+        }
+    }
+
+    public void paint(Graphics g){
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+
+
+        int centroX = getWidth() / 2;
+        int centroY = getHeight() / 2;
+        g2d.translate(centroX, centroY);
+
+        g2d.setColor(Color.LIGHT_GRAY);
+        g2d.drawLine(-centroX, 0, centroX, 0);
+        g2d.drawLine(-centroY, 0, centroY, 0);
+
+        g2d.setColor(Color.RED);
+        int xv = (int) (equacao.getXVertice() * 50);
+        int yv = (int) (equacao.getYVertice() * -50);
+        g2d.fillOval(xv - 5, yv - 5, 10, 10);
+        g2d.drawString("Vertice", xv + 10, yv);
+
+
+    }
+}
+
